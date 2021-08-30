f7d4646b156b1c35cc171537d683918c2d26e85c
OhMesch <mze5111@gmail.com>
2021-08-30 11:28:52 -0400

    No merge prompt

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@ then
 else
     echo "Updating..."
     killall -e "python3"
-    git merge origin/main
+    git merge origin/main --no-edit
     python3 ~/Teams-Status-Indicator/main.py #Restart application
     echo "Update complete."
 fi
